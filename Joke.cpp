da1f813fc3fa69b2e4a6f0a8c0f64d22731a3cf1
Pr0tag0n1st <665825@dpsk12.net>
2018-01-17 08:00:54 -0700

    This is a joke about airplane security

---
+++ Joke.cpp
@@ -0,0 +1,7 @@
+//made by TJ and Arnulfo
+#include<iostream>
+using namespace std;
+int main() {
+	cout << "What don't you say in a airplane instead of bomb?" << endl << endl << endl << endl << endl;
+	cout << "Hi Jack!" << endl;
+}
